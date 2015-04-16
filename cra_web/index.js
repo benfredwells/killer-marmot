0df3db8a24a971a57ef7026057fc4fba9baa2aab
Ben Wells <benwells@chromium.org>
2015-04-16 19:37:03 +1000

    Update text

--- cra_web/index.js
+++ cra_web/index.js
@@ -11,7 +11,7 @@ if ('serviceWorker' in navigator) {
 window.addEventListener("beforeinstallprompt", function(e) {
   document.open();
   document.write('Got beforeinstallprompt!!!<br>');
-  document.write('platform: ');
+  document.write('platforms: ');
   document.write(e.platforms);
   document.write('<br>Should I cancel it? Hmmmm .... ');
   if (Math.random() > 0.5) {
