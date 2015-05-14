76855733521929d95dd7c74d22caa983714e5fd7
Dominick Ng <dominickn@chromium.org>
2015-05-14 11:28:38 +1000

    * adding a test for redispatching events

---
+++ web_redispatch/index.js
@@ -0,0 +1,23 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+var isTooSoon = (window.location.hash == "#redispatch");
+window.addEventListener("beforeinstallprompt", function(e) {
+  if (isTooSoon) {
+    e.preventDefault(); // Prevents prompt display
+    console.log("Delaying event!");
+    // Prompt later instead:
+    setTimeout(function() {
+      isTooSoon = false;
+      console.log("Dispatching event");
+      window.dispatchEvent(e); // Shows prompt
+    }, 5000);
+  }
+});
