7defe018b0f8f952912221e2f7820f06cd970f26
Dominick Ng <dominickn@chromium.org>
2015-05-21 16:14:19 +1000

    * adding a directory with no meta viewport tag

---
+++ web_no_meta_viewport/sw.js
@@ -0,0 +1,20 @@
+importScripts('../serviceworker-cache-polyfill.js');
+
+var CACHE_NAME = 'killer-marmot-v2';
+var urlsToCache = [
+//  'index.js',
+//  'index.html'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
