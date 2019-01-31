73e5594df3807371add9d4e22801ff2faad8338e
Jay Harris <jay.harris@outlook.co.nz>
2019-01-31 14:10:36 +1100

    Adds a sub resource service worker

---
+++ static/sw-maskable.js
@@ -0,0 +1,32 @@
+importScripts('serviceworker-cache-polyfill.js');
+
+// Note: There is no reason for this ServiceWorker to be it's own thing, we could just
+// cache this stuff dynamically in the parent ServiceWorker, but this is used to test
+// nested service workers.
+let CACHE_NAME = 'killer-marmot-maskable-v1';
+let urlsToCache = [
+ 'maskable.html',
+ 'style.css',
+ 'marmot_96.png',
+ 'maskable.png',
+ 'maskable-measurements.svg',
+ 'marmot_48.png',
+ 'marmot.png'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened maskable cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Required to be installable.
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request)
+        .then(response => response || fetch(event.request)));
+});
